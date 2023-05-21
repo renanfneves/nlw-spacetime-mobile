782f033a84c5b2685dea1f0490ae1fc2880ee8ae
Renan Floriano Neves <renanfneves@gmail.com>
2023-05-21 17:09:16 +0100

    [feature]: add layout and stack navigation to the project

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,11 @@
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import * as SecureStore from 'expo-secure-store'
-import { styled } from 'nativewind'
 import { useEffect } from 'react'
 import { useRouter } from 'expo-router'
 
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
-
-import blurBg from '../src/assets/bg-blur.png'
-import Stripes from '../src/assets/stripes.svg'
 import NlWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { api } from '../src/lib/api'
-
-// Nativewind doesn't apply css classes naturally to a no global element (Ex.: View)
-// To solve it, it is needed to encapsule the element within styled function
-const StyledStripes = styled(Stripes)
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -44,12 +28,6 @@ export default function App() {
     discovery,
   )
 
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
-
   async function handleGithubOAuthCode(code: string) {
     const response = await api.post('/register', {
       code,
@@ -71,6 +49,7 @@ export default function App() {
     //     scheme: 'nlwspacetime',
     //   }),
     // )
+
     if (response?.type === 'success') {
       const { code } = response.params
 
@@ -78,21 +57,8 @@ export default function App() {
     }
   }, [response])
 
-  if (!hasLoadedFonts) {
-    return null
-  }
-
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{
-        position: 'absolute',
-        left: '-100%',
-      }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className=" flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6">
         <NlWLogo />
 
@@ -120,8 +86,6 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
         Feito com 💜 no NLW da Rocketseat
       </Text>
-
-      <StatusBar style="auto" />
-    </ImageBackground>
+    </View>
   )
 }
